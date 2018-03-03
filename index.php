f670c1045450d1f5b6bf47b5a05a047320c4129f
Dave <dave@wakesys.com>
2018-03-03 01:41:16 +0100

    asd

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
-
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
 echo '<style>';
 include('css/normalize.min.css');
 include('css/skeleton.min.css');
 echo '</style>';
+echo '</head>';
 
+echo '<body>';
 include('../parsedown/Parsedown.php');
 $Parsedown = new Parsedown();
 
-echo $Parsedown->text('Hello _Parsedown_!');
+
 
 $file_name = "content/table.md";
 $myfile = fopen($file_name, "r") or die("Unable to open file!");
 $file_content = fread($myfile, filesize($file_name));
 fclose($myfile);
 
+echo '<div class="container">';
+echo '<h1>Heading</h1>';
+echo $Parsedown->text('Hello _Parsedown_!');
 echo $Parsedown->text($file_content);
-
+echo '</div>';
+echo '</body>';
+echo '</html>';
 ?>
