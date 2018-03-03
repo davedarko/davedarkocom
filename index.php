9aa25bf4401ac3d4f56eaffd3e0837cd57d13f1b
Dave <dave@wakesys.com>
2018-03-03 16:00:30 +0100

    d

--- index.php
+++ index.php
@@ -65,6 +65,9 @@ function scan_and_list_folder($dir)
 	$content_folder = scandir($dir);
 	if (is_array($content_folder))
 	{
+		echo '<h2>';
+		echo make_that_filename_pretty($dir);
+		echo '</h2>';
 		echo '<ul>';
 		foreach ($content_folder as $file_name)
 		{
