123862b1387e3c17241166527f9f08cca03a217a
Dave <dave@wakesys.com>
2018-03-03 18:04:39 +0100

    all content converted yaaay

--- content/old_blog/271220102310.php
+++
@@ -1,5 +0,0 @@
-<h2>Skateboard M&ouml;bel</h2>
-27.12.2010 - 23:10<p>
-<a href="http://www.toxel.com/inspiration/2009/01/28/skateboard-inspired-furniture-designs/">M&ouml;bel</a><br />
-<br />
-Ich liebe es. Wenn ich mir meinen PC L&uuml;fter besorgt habe, bau ich mir eine RGB Skateboardlampe. Und ich werd mir die Sachen einfach fertig kaufen und nur noch zusammenschrauben. Dann kann ich dahinter einen Haken setzten (Projekt fertig).<p>Tags: rgb, skateboard, m&ouml;bel
--- content/old_blog/281220102357.php
+++
@@ -1,7 +0,0 @@
-<h2>Jugendmedienschutz und Personalausweis</h2>
-28.12.2010 - 23:57<p>
-Aufgrund der inzwischen gescheiterten Novellierung des jmstv besch&auml;ftige ich mich gerade mit Altersverifizierung und Personalausweisnummern und zeitlichen Abfragen etc. pp. Ich ahnte eine dunkle Zukunft vorraus, in der das jmstv f&uuml;r eine neue Abmahnwelle &auml;hnlich der torrent-Klagen benutzt wird. Jedenfalls f&auml;llt die Altersbestimmung &uuml;ber den Personalausweis definitiv aus, da der Algorythmus viel zu einfach ist. <br />
-<br />
-<a href="http://de.wikipedia.org/wiki/Ausweisnummer">http://de.wikipedia.org/wiki/Ausweisnummer</a><br />
-<a href="http://de.wikipedia.org/wiki/Jugendmedienschutz-Staatsvertrag">http://de.wikipedia.org/wiki/Jugendmedienschutz-Staatsvertrag</a><br />
-<a href="http://t3n.de/news/neuer-jmstv-286977/">http://t3n.de/news/neuer-jmstv-286977/</a><p>Tags: jmstv
