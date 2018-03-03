a239801c9a4069cd214d2c43e0e90bb076ddb998
Dave <dave@wakesys.com>
2018-03-03 18:46:00 +0100

    applet link

--- content/old_blog/201112270407_DE_Game_Boy_Printer_IX_-_Applet_Update.php
+++ content/old_blog/201112270407_DE_Game_Boy_Printer_IX_-_Applet_Update.php
@@ -1,4 +1,4 @@
 <h2>Gameboy Printer IX - Applet Update</h2>
 27.12.2011 - 04:07<p>
-<a href="http://davedarko.com/gameboy.php">http://davedarko.com/gameboy.php</a> <br />
+<a href="http://davedarko.com/gameboy-php/gameboy.php">http://davedarko.com/gameboy-php/gameboy.php</a> <br />
 Woohoo! Aus den Textdateien werden nun endlich Bilddateien generiert! Es sind indizierte pngs, sodass die Daten Menge klein gehalten werden kann. Sp&auml;ter werden diese zum Drucken mit dem Gameboy Drucker wieder aufger&ouml;delt.<p> <p>Tags: Gameboy Printer, Applet
