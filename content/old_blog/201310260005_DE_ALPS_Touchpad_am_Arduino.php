3d3ca4210ff1128834d6a7e7fb48f82cc4604a24
Dave <dave@wakesys.com>
2018-03-03 18:12:08 +0100

    hr

--- content/old_blog/201310260005_DE_ALPS_Touchpad_am_Arduino.php
+++ content/old_blog/201310260005_DE_ALPS_Touchpad_am_Arduino.php
@@ -5,6 +5,7 @@ W&auml;hrend meiner Sortier und Aufr&auml;umaktionen, die meistens eher im Chaos
 Ein wenig schlau googlen und ich fand heraus, dass die meisten Touchpads wohl &uuml;ber PS / 2 kommunizieren. Ein Protokoll was ich des &ouml;fteren auf der Hackaday Seite in Benutzung mit Mikrocontrollern sah und schnell die ersten Projekte fand.
 <h2>Pinbelegung</h2>
 Anhand der unterschiedlichen ALPS Pads und dem Oscillator dessen Mittelbein gegen Masse geht und meinem Multimeter auf Widerstandsmessung eingestellt, konnte ich die Pinbelegung dann doch schnell bestimmen. Mein reaktiviertes Atmega8 Board, welches ich Arduino IDE f&auml;hig l&ouml;tete sendet mir nun &uuml;ber die serielle Schnittstelle die X und Y Differenzen, die mein Fingerstreifen ausl&ouml;sen.
+<hr>
 <a rel="facebox" href="../img/20131026_0009292013-10-26.jpg"><img src="../img/th_20131026_0009292013-10-26.jpg"></a>
 <a rel="facebox" href="../img/20131026_0007452013-10-26.jpg"><img src="../img/th_20131026_0007452013-10-26.jpg"></a>
 <h2>Die wichtigsten Links</h2>
