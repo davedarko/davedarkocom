2d2b7fe4a0f7c7a2860192591bf9bc0856853864
Dave <dave@wakesys.com>
2018-03-03 17:59:00 +0100

    ikea regal

--- content/old_blog/201208271828_DE_RGB_IKEA_Regal.php
+++ content/old_blog/201208271828_DE_RGB_IKEA_Regal.php
@@ -2,15 +2,23 @@
 27.08.2012 - 18:28<p>
 Nachdem ich mir wortw&ouml;rtlich die Z&auml;hne an diesem Projekt ausgebissen habe, muss es hier auch endlich Erw&auml;hnung finden. Urspr&uuml;nglich sollte mein Raumteiler - ein 5x5 Regal vom schwedischen M&ouml;belhinterherwerfer mit 25 RGB Modulen ausgetattet werden. Bisher sind es erst 9. Die Basis/ das Vorbild f&uuml;r den Aufbau und den Code sind BlinkM Lights von ThingM.<p> <h2>Die Platine</h2><br />
 Eine Platine ist eigentlich unn&ouml;tig, da man alle Komponenten auch im deadbug Stil h&auml;tte zusammen bringen k&ouml;nnen. Da ich eh schon immer einmal eigene Platinen &auml;tzen wollte, habe ich mit Hilfe von Inkscape ein Layout erstellt und im CopyShop meines Vertrauens auf geeignetes Paper gebracht. Ich entschied mich f&uuml;r die Tonertransfer Methode. Bei reichelt erstand ich ein Einstiegsset f&uuml;r Platinen&auml;tzen und folgte der Anleitung. Auf dem Bild ist meine allererste Platine zu sehen.<br />
-<a rel="facebox" href="pics/IMG_20120721_1521482012-08-07.jpg"><br />
-<img src="http://davedarko.com/pics/th_IMG_20120721_1521482012-08-07.jpg"></a><br />
+<a rel="facebox" href="../img/IMG_20120721_1521482012-08-07.jpg">
+<img src="../img/th_IMG_20120721_1521482012-08-07.jpg">
+</a><br />
 <br />
 <h2>Der Erste und der Rest</h2><br />
 Der Code entstammt dem Ghetto Pixels Projekt auf <a href="http://www.instructables.com/id/Ghetto-Pixels-Building-an-open-source-BlinkM/">http://www.instructables.com/id/Ghetto-Pixels-Building-an-open-source-BlinkM/</a>. Der f&uuml;r gemeinesame Kathode umgeschriebene Code erf&uuml;llte wunderbar seinen Zweck. In der ersten Version benutzte ich noch den BlinkM Code und invertierte den Farbwert. Das Ergebnis entsprach aber nicht meiner Vorstellung und die Lampen liessen sich auch nicht ausschalten. Auch hatte ich vor dem Einl&ouml;ten bei den meissten Lampen vergessen, den Fuse f&uuml;r den OSC OUT abzuschalten, so dass die gr&uuml;ne Lampe st&auml;ndig leuchtete. Es musste eine L&ouml;sung her, die sich mir nur in vorm einer selbstgebastelten SPI-W&auml;scheklammer Adapter bot. <br />
 <br />
-<a rel="facebox" href="pics/IMG_20120822_1352542012-08-27.jpg"><img src="http://davedarko.com/pics/th_IMG_20120822_1352542012-08-27.jpg"></a>
-<a rel="facebox" href="pics/IMG_20120723_1441462012-08-07.jpg"><img src="http://davedarko.com/pics/th_IMG_20120723_1441462012-08-07.jpg"></a> 
-<a rel="facebox" href="pics/IMG_20120731_1816372012-08-07.jpg"><img src="http://davedarko.com/pics/th_IMG_20120731_1816372012-08-07.jpg"></a><br />
+<a rel="facebox" href="../img/IMG_20120822_1352542012-08-27.jpg">
+	<img src="../img/th_IMG_20120822_1352542012-08-27.jpg">
+</a>
+<a rel="facebox" href="../img/IMG_20120723_1441462012-08-07.jpg">
+	<img src="../img/th_IMG_20120723_1441462012-08-07.jpg">
+</a> 
+<a rel="facebox" href="../img/IMG_20120731_1816372012-08-07.jpg">
+	<img src="../img/th_IMG_20120731_1816372012-08-07.jpg">
+</a>
+<br />
 <br />
 Nach all dem Hickhack mit den falschen Adressen, der falschen Software und den nicht richtig gesetzten fuses wollte ich schon drei Kreutze machen, aber es gelang mir dann doch ein breadboard-approved-proof-of-concept zu basteln. <br />
 <br />
