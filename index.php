3836419916ee7eac867c0266aedff8c9907e6393
Dave <dave@wakesys.com>
2018-03-03 01:34:29 +0100

    markdown file loading test

--- index.php
+++ index.php
@@ -10,4 +10,11 @@ $Parsedown = new Parsedown();
 
 echo $Parsedown->text('Hello _Parsedown_!');
 
+$file_name = "content/table.md";
+$myfile = fopen($file_name, "r") or die("Unable to open file!");
+$file_content = fread($myfile, filesize($file_name));
+fclose($myfile);
+
+echo $Parsedown->text($file_content);
+
 ?>
