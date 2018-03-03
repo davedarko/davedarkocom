423908d6a96f0e3298a5cca854fa477f7a493ee3
Dave <dave@wakesys.com>
2018-03-03 01:22:56 +0100

    hmm

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="../Skeleton/css/normalize.css">
+<link rel="stylesheet" href="../Skeleton/css/skeleton.css">
 <?php
 include('../parsedown/Parsedown.php');
 $Parsedown = new Parsedown();
