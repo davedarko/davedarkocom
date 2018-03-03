4bbc076c274faa00a81bc3b1400fa4af1ec79990
Dave <dave@wakesys.com>
2018-03-03 17:49:22 +0100

    content content content

--- content/old_blog/201311241006_DE_Game_Boy_Printer_XI_Erfolg.php
+++ content/old_blog/201311241006_DE_Game_Boy_Printer_XI_Erfolg.php
@@ -1,4 +1,4 @@
-<h2>Gameboy Printer XI - Success</h2>
+<h2>Game Boy Printer XI - Erfolg</h2>
 24.11.2013 - 10:06<p>
 Nachdem meine Bilder irgendwie doch ziemlich falsch formatiert waren, habe ich mit php den Export nach den neuesten Erkenntnissen variiert. <br />
 Beim eintragen in den Arduino Code und &uuml;bertragen sind mir meine zwei Import Nanos immer abgeschmiert, bis ich feststellen musste, dass die dann tats&auml;chlich keine 30000 bytes sondern nur 6500 bytes speichern k&ouml;nnen. Im allgemeinen weniger ein Problem, da meine Skripte eh nicht so gro&szlig; werden, aber doch schon ein wenig frech. Wer allerdings ein 5tel bezahlt, sollte sich &uuml;ber ein 5tel Leistung nicht beschweren.<br />
@@ -10,7 +10,7 @@ Mein erster Ansatz zum Bilder speichern erfolgte pro Zeile mit dem Wert 0,1,2,3 
 <li>2 Zeilen f&uuml;r einen Informationsschub</li>
 </ul>
 <strong>2 Bytes * 8 Linien * 20 Bl&ouml;cke * 2 Zeilen = 640 Bytes</strong><p>
-<a rel="facebox" href="pics/20131124_0038072013-11-24.jpg"><img src="../img/th_20131124_0038072013-11-24.jpg"></a> 
+<a rel="facebox" href="../img/20131124_0038072013-11-24.jpg"><img src="../img/th_20131124_0038072013-11-24.jpg"></a> 
 <img src="gbapp_converter.php?file=./gameboyapp/20120101044519-TUXIES.dat&color=ffddbb">
 <br />
 <p>Tags: gameboy printer
